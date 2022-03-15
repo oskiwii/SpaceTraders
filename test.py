c4c5235045a807cae4cf741dfa479b34d46d45a6
oskiwii <80816818+oskiwii@users.noreply.github.com>
2022-03-15 16:28:36 +0000

    Update test.py  (wow i should hide that token)

--- test.py
+++ test.py
@@ -1,16 +1,40 @@
 import asyncio
+import pprint
 from spt import Client
 
-cli = Client('eb085f34-e8f1-4a5a-93ab-f9c337749dce', verbose = True, max_retries = 5)
+import logging
+logger = logging.getLogger('spacetraders-http')
+logger.setLevel(logging.INFO)
+fh = logging.StreamHandler()
+formatter = logging.Formatter('[%(name)s]  [%(levelname)s]  %(message)s')
+fh.setFormatter(formatter)
+fh.setLevel(logging.INFO)
+logger.addHandler(fh)
+
+cli = Client('eb085f34-e8f1-4a5a-93ab-f9c337749dce', verbose = True, max_retries = 5, timeout = 45)
+
+
+@cli.event(event_name='on_ready')
+async def on_ready():
+    logger.info('Client appears ready to start!')
+
+
 
 
 async def main():
-    await cli.test()
-    await cli.test()
-    await cli.test()
-    await cli.test()
-    await cli.test()
-    await cli.test()
-    await cli.test()
 
-loop = asyncio.get_event_loop().run_until_complete(main())
+    online = await cli.online
+    if not online:
+        raise Exception('spacetraders is not available')
+    
+    print((await cli.account).name)
+
+    
+
+
+    return
+
+
+cli.start(main)
+input('ENTER to exit: ')
+
